23af339dfb3899ff6acf5cf7f5f5e8fed18c0383
Fombi-Favour <fombifavour@gmail.com>
2023-08-01 11:54:30 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { FaMicrophone, FaRegSun } from 'react-icons/fa';
 
 const Navbar = () => (
-  <nav>
-    <span>The World Map</span>
-    <span>COVID-19 Cases</span>
-    <div>
+  <nav className="bg-[#ec4c8b] flex items-center justify-between p-3 md:justify-around text-white">
+    <span className="font-bold">The World Map</span>
+    <span className="text-lg font-medium">COVID-19 Cases</span>
+    <div className="flex items-center gap-3 md:text-xl">
       <FaMicrophone />
       <FaRegSun />
     </div>
