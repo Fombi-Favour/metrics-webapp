5311bc2f5e00b00ee23d8ebb393c45a839d82b0c
Fombi-Favour <fombifavour@gmail.com>
2023-08-01 16:46:26 +0100

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchData } from '../redux/home/homeSlice';
 
-const Home = () => (
-  <div>Home</div>
-);
+const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
+  return (
+    <div>Home</div>
+  );
+};
 
 export default Home;
