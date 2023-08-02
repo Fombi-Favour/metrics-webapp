2906a3e3f3d1162cd1638b8b2b2c9d082a8882a4
Fombi-Favour <fombifavour@gmail.com>
2023-08-02 23:15:06 +0100

    Update homeSlice.js

--- src/redux/home/homeSlice.js
+++ src/redux/home/homeSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { v4 as uuid } from 'uuid';
 
 const initialState = {
   contents: [],
@@ -6,16 +7,30 @@ const initialState = {
 };
 const url = 'https://corona.lmao.ninja/v2/continents';
 
-export const fetchContinents = createAsyncThunk('home/fetchContinent', async () => {
+export const fetchContinents = createAsyncThunk('home/fetchContinents', async () => {
   const response = await fetch(url);
   const res = await response.json();
   return res.map((item) => ({
+    id: uuid(),
     name: item.continent,
     population: item.population,
     cases: item.cases,
     deaths: item.deaths,
     recovered: item.recovered,
   }));
+});
+
+export const fetchContinent = createAsyncThunk('home/fetchContinent', async (continentName) => {
+  const base = `${url}/${continentName}`;
+  // const response = await fetch(base);
+  // const newContinent = {
+  //   name: response.continent,
+  //   population: response.population,
+  //   cases: response.cases,
+  //   deaths: response.deaths,
+  //   recovered: response.recovered,
+  // };
+  console.log(base);
 });
 
 const homeSlice = createSlice({
