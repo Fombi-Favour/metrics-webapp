e99d2b20aa1422a6bf8cbfc4372aff6c8c2191f9
Fombi-Favour <fombifavour@gmail.com>
2023-08-02 14:50:27 +0100

    Create Continent.jsx

---
+++ src/components/Continent.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { FaArrowCircleRight } from 'react-icons/fa';
+import imageContinent from './Image';
+
+const Continent = ({ continent }) => {
+  const {
+    name, population, cases, deaths, recovered,
+  } = continent;
+  return (
+    <NavLink to="details" className="flex flex-col items-end border-2 p-2">
+      <div>
+        <FaArrowCircleRight />
+      </div>
+      <div className="flex items-center gap-1">
+        <img src={imageContinent[name]} alt={name} className="w-48" />
+        <div className="flex flex-col">
+          <h3 className="font-bold text-xl">{name}</h3>
+          <span>
+            Population:
+            {' '}
+            {population}
+          </span>
+          <span>
+            Cases:
+            {' '}
+            {cases}
+          </span>
+          <span>
+            Deaths:
+            {' '}
+            {deaths}
+          </span>
+          <span>
+            Recovered:
+            {' '}
+            {recovered}
+          </span>
+        </div>
+      </div>
+    </NavLink>
+  );
+};
+
+Continent.propTypes = {
+  continent: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+export default Continent;
