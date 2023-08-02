dc1c2bef819e26c62d8d6e4026f9d210fa0dacb2
Fombi-Favour <fombifavour@gmail.com>
2023-08-02 10:16:12 +0100

    Update homeSlice.js

--- src/redux/home/homeSlice.js
+++ src/redux/home/homeSlice.js
@@ -1,17 +1,28 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const initialState = [];
+const initialState = {
+  contents: [],
+  isloading: false,
+};
 const url = 'https://corona.lmao.ninja/v2/continents';
 
-export const fetchData = createAsyncThunk('home/fetchContinent', async () => {
+export const fetchContinents = createAsyncThunk('home/fetchContinent', async () => {
   const response = await fetch(url);
   const res = await response.json();
   console.log(res);
+  return res.map((item) => ({
+    name: item.continent,
+    population: item.population,
+    cases: item.cases,
+    deaths: item.deaths,
+    recovered: DataTransferItemList.recovered,
+  }));
 });
 
 const homeSlice = createSlice({
   name: 'home',
   initialState,
+  reducers: {},
 });
 
 export default homeSlice.reducer;
