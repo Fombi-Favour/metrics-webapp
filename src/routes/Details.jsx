eeaf58061e43bea15f832888a1e4d81e66bf8ee3
Fombi-Favour <fombifavour@gmail.com>
2023-08-03 10:45:21 +0100

    Update Details.jsx

--- src/routes/Details.jsx
+++ src/routes/Details.jsx
@@ -1,17 +1,40 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import { fetchContinent } from '../redux/home/homeSlice';
+import imageContinents from '../components/Image';
 
 const Details = () => {
   const dispatch = useDispatch();
-  // const { value } = useSelector((state) => state.details);
+  const { name } = useParams();
+  const { continent } = useSelector((state) => state.home);
+  console.log(name, continent);
 
   useEffect(() => {
-    dispatch(fetchContinent());
+    dispatch(fetchContinent(name));
   }, [dispatch]);
 
   return (
-    <div>Details</div>
+    <section className="bg-[#fb5092] text-white">
+      {/* Title sub-section */}
+      <div>
+        <img src={imageContinents[name]} alt={name} className="w-80 md:w-[24rem]" />
+        <div className="flex flex-col">
+          <h3 className="text-[1.654rem] font-semibold">{continent.name}</h3>
+          <span className="">
+            {continent.cases}
+            {' '}
+            cases
+          </span>
+        </div>
+      </div>
+      {/* status sub-section */}
+      <div className="flex bg-[#da2d72] p-1">
+        <h3 className="uppercase">
+          {`status of ${continent.name}'s countries`}
+        </h3>
+      </div>
+    </section>
   );
 };
 
