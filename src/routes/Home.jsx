1171d6c4712644dedb2bdc07bedc9c82f44fdb0e
Fombi-Favour <fombifavour@gmail.com>
2023-08-03 22:51:24 +0100

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,12 +6,18 @@ import Continent from '../components/Continent';
 
 const Home = () => {
   const [search, setSearch] = useState('');
+  const [total, setTotal] = useState(0);
   const dispatch = useDispatch();
   const { contents } = useSelector((state) => state.home);
 
   useEffect(() => {
     dispatch(fetchContinents());
   }, [dispatch]);
+
+  useEffect(() => {
+    const totalCase = contents?.reduce((prev, curr) => prev + curr.cases, 0);
+    setTotal(totalCase);
+  }, [contents]);
 
   const filteredContinent = contents.filter((item) => (
     item.name.toLowerCase().includes(search.toLowerCase())
@@ -24,18 +30,22 @@ const Home = () => {
         <img src={World} alt="world-map" className="w-48 md:w-[24rem]" />
         <div className="flex flex-col">
           <h3 className="text-[1.654rem] font-semibold">The World</h3>
-          <span className="text-[1.174rem]">6,958 cases</span>
+          <span className="text-[1.174rem]">
+            {total}
+            {' '}
+            cases
+          </span>
         </div>
       </div>
       {/* searching sub-section */}
-      <div className="flex items-center justify-between bg-[#da2d72] py-3 px-8 md:justify-evenly">
+      <div className="flex items-center justify-between bg-[#da2d72] py-3 px-2 md:justify-evenly">
         <h3 className="uppercase">cases by continents</h3>
         <input
           type="text"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           placeholder="Search..."
-          className="w-60 md:w-[30rem] p-1 px-3 outline-none text-black rounded-md"
+          className="w-48 md:w-[30rem] p-1 px-3 outline-none text-black rounded-md"
         />
       </div>
       {/* listing sub-section: fetched from api */}
