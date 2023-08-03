0b4331a53c08cced22aa229bbd952324096fd37c
Fombi-Favour <fombifavour@gmail.com>
2023-08-03 19:58:15 +0100

    Update Country.jsx

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Country = () => (
-  <div>Country</div>
-);
+const Country = ({ country }) => {
+  const {
+    name, flag, continent, population,
+  } = country;
+
+  return (
+    <div>
+      <span>{name}</span>
+      <span>{flag}</span>
+      <span>{continent}</span>
+      <span>{population}</span>
+    </div>
+  );
+};
+
+Country.propTypes = {
+  country: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    flag: PropTypes.string.isRequired,
+    continent: PropTypes.string.isRequired,
+    population: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default Country;
