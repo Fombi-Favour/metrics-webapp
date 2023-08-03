a03620706e456a75185f4969f778b62daf64ee2b
Fombi-Favour <fombifavour@gmail.com>
2023-08-03 21:58:33 +0100

    Update Country.jsx

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FaArrowCircleRight } from 'react-icons/fa';
 
 const Country = ({ country }) => {
   const {
-    name, flag, continent, population,
+    name, flag, population, cases, deaths, recovered,
   } = country;
 
   return (
-    <div>
-      <span>{name}</span>
-      <span>{flag}</span>
-      <span>{continent}</span>
-      <span>{population}</span>
-    </div>
+    <main className="flex items-center gap-12 md:justify-between px-2 py-3 bg-[#fb5092] even:bg-[#da2d72]">
+      <div className="flex items-center gap-2">
+        <img src={flag} alt={country} className="w-16 md:w-40" />
+        <span className="text-lg font-semibold">{name}</span>
+      </div>
+      <div className="flex">
+        <div className="flex flex-col">
+          <span>
+            Population:
+            {' '}
+            {population}
+          </span>
+          <span>
+            Cases:
+            {' '}
+            {cases}
+            {' '}
+            cases
+          </span>
+          <span>
+            Deaths:
+            {' '}
+            {deaths}
+            {' '}
+            deaths
+          </span>
+          <span>
+            Recovered:
+            {' '}
+            {recovered}
+            {' '}
+            recovered
+          </span>
+        </div>
+        <div>
+          <FaArrowCircleRight />
+        </div>
+      </div>
+    </main>
   );
 };
 
@@ -20,8 +54,10 @@ Country.propTypes = {
   country: PropTypes.shape({
     name: PropTypes.string.isRequired,
     flag: PropTypes.string.isRequired,
-    continent: PropTypes.string.isRequired,
     population: PropTypes.number.isRequired,
+    cases: PropTypes.number.isRequired,
+    deaths: PropTypes.number.isRequired,
+    recovered: PropTypes.number.isRequired,
   }).isRequired,
 };
 
