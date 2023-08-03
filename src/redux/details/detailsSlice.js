7dab53f1fd54b3f4bec74cdc2c59a1db4ba10aa7
Fombi-Favour <fombifavour@gmail.com>
2023-08-03 19:58:07 +0100

    Update detailsSlice.js

--- src/redux/details/detailsSlice.js
+++ src/redux/details/detailsSlice.js
@@ -1,24 +1,16 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
-  value: [],
+  valueList: [],
   isloading: false,
 };
 const url = 'https://corona.lmao.ninja/v2/countries/';
 
-export const fetchCountries = createAsyncThunk('details/fetchCountries', async (countries) => {
-  const base = `${url}/${countries}`;
+export const fetchCountries = createAsyncThunk('details/fetchCountries', async (name) => {
+  const base = `${url}${name}`;
   const response = await fetch(base);
   const res = await response.json();
-  return res.forEach((item) => ({
-    country: item.country,
-    flag: item.countryInfo.flag,
-    continent: item.continent,
-    populations: item.population,
-    cases: item.cases,
-    deaths: item.deaths,
-    recovered: item.recovered,
-  }));
+  return { ...res, name };
 });
 
 const detailsSlice = createSlice({
@@ -33,7 +25,7 @@ const detailsSlice = createSlice({
       .addCase(fetchCountries.fulfilled, (state, action) => ({
         ...state,
         isloading: false,
-        value: action.payload,
+        valueList: action.payload,
       }))
       .addCase(fetchCountries.rejected, (state) => ({
         ...state,
