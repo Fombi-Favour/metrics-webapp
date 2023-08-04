7463a04c3aed74a21ae59bdebf2298d5ae8c0a52
Fombi-Favour <fombifavour@gmail.com>
2023-08-04 13:11:22 +0100

    Update HomeSlice.test.jsx

--- src/tests/HomeSlice.test.jsx
+++ src/tests/HomeSlice.test.jsx
@@ -3,12 +3,18 @@ import homeSlice, { fetchContinents } from '../redux/home/homeSlice';
 
 describe('testing homeslice', () => {
   it('should set isLoading to false and contents to the fetched data when fetchContinents.fulfilled is called', () => {
+    const state = {
+      contents: [],
+      continent: [],
+      isloading: true,
+    };
     const data = [{ name: 'Asia', population: 1000 }];
-    const state = homeSlice.reducer(
-      data,
-      fetchContinents.fulfilled(data),
-    );
-    expect(state.isLoading).toBe(false);
-    expect(state.contents).toEqual(data);
+    const action = { type: fetchContinents.fulfilled.type, payload: data };
+    const expectedState = {
+      contents: data,
+      continent: [],
+      isloading: false,
+    };
+    expect(homeSlice(state, action)).toEqual(expectedState);
   });
 });
